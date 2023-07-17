97bfb39ff499c3f1f15c02bb9305ecd3b12e7ad4
habibaadam <habiepalmer@gmail.com>
2023-07-17 02:13:48 -0700

    function prototypes introduced

--- shell.h
+++ shell.h
@@ -1,6 +1,37 @@
 #define SHELL_H
 #ifndef SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#define TOK_DEL "\n\t\r\a"
+#define CMD_LEN 64
+#define BUF 1024
+
+
+extern char **environ;
+
+/*Shell's Loop*/
+void loop_ourshell();
+
+/*Command/Input Readers */
+char *read_command();
+
+/*Token Handlers */
+char **token_maker(char *linebuf);
+
+/* Execution Hnadlers */
+int exec_args();
+
+
+
+
+/* All String Function Implementations */
+
 int strlen_alt(const char *s);
 char *strcat_alt(char *dest, char *src);
 int strcmp_alt(char *s1, char *s2);
