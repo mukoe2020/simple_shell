8f18868e331af7cd30a5146338392bc18976ed10
memoe <memoemukoe@gmail.com>
2023-07-26 04:39:17 -0700

    fixing

--- shell_main.c
+++ shell_main.c
@@ -37,11 +37,10 @@ void loop_ourshell(int ac, char **argv)
   */
 int main(int ac __attribute__((unused)), char **argv __attribute__((unused)))
 {
-	setenv("PATH", "/usr/bin", 1);
 
 	loop_ourshell(ac, argv);
 
-	return (EXIT_SUCCESS);
+	return (0);
 }
 
 /**
